81b95144edfa00ab8c9a21d4841003082200c812
MrK3w <jakubpietrus2000@gmail.com>
2021-02-03 19:32:22 +0100

    changed to correct naming conventions

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -1,33 +1,37 @@
-from os import system, name 
+from os import system, name
+
 gameBoard = []
 
-def clear(): 
-  
-    # for windows 
-    if name == 'nt': 
-        _ = system('cls') 
-  
-    # for mac and linux(here, os.name is 'posix') 
-    else: 
-        _ = system('clear') 
 
-def printGameBoard(listOfSigns):
+def clear():
+    # for windows 
+    if name == 'nt':
+        _ = system('cls')
+
+        # for mac and linux(here, os.name is 'posix') 
+    else:
+        _ = system('clear')
+
+
+def print_game_board(list_of_signs):
     clear()
 
     i = 0
     j = 0
     print("#|0|1|2|")
     print('-' * 8)
-    while(i < 3):
-        print(f"{i}|",end = "")
-        while(j < 3):
-            print(listOfSigns[i][j],end = "|")
+    while i < 3:
+        print(f"{i}|", end="")
+        while j < 3:
+            print(list_of_signs[i][j], end="|")
             j += 1
         print('\n' + '-' * 8)
         i += 1
         j = 0
-    
-def getInput():
+
+
+def get_input():
+    position = 0
     while True:
         try:
             position = int(input("Please enter your position 0-2: "))
@@ -38,19 +42,19 @@ def getInput():
             break
     return position
 
-def UserInput():
+
+def user_input():
     while True:
-        positions = []
-        positions.append(getInput())
-        positions.append(getInput())
-        
-        if IsPositionValid(positions): 
-            break 
+        positions = [get_input(), get_input()]
+
+        if is_position_valid(positions):
+            break
         else:
             print("Bad position")
     return positions
 
-def IsBoardFull():
+
+def is_it_draw():
     for oneList in gameBoard:
         if ' ' in oneList:
             return False
@@ -58,7 +62,8 @@ def IsBoardFull():
         print("It's a draw!")
         return True
 
-def CheckLine():
+
+def check_line():
     i = 0
     j = 0
     while j < 3:
@@ -72,21 +77,24 @@ def CheckLine():
         i = 0
     return False
 
-def CheckDiagonal():
-    firstCross = [gameBoard[0][0],gameBoard[1][1], gameBoard[2][2]]
-    secondCross = [gameBoard[0][2],gameBoard[1][1], gameBoard[2][0]]
-    if (len(set(firstCross)) == 1 and ' ' not in firstCross) or (len(set(secondCross)) == 1 and ' ' not in secondCross):
-        return True
-    return False
 
-def CheckRow():
+def check_diagonal():
+    first_cross = [gameBoard[0][0], gameBoard[1][1], gameBoard[2][2]]
+    second_cross = [gameBoard[0][2], gameBoard[1][1], gameBoard[2][0]]
+    if (len(set(first_cross)) != 1 or ' ' in first_cross) and (len(set(second_cross)) != 1 or ' ' in second_cross):
+        return False
+    return True
+
+
+def check_row():
     for boardLine in gameBoard:
         if len(set(boardLine)) == 1 and ' ' not in boardLine:
             return True
 
-def IsItWin(playerOne):
-    if CheckRow() or CheckLine() or CheckDiagonal():
-        if playerOne:
+
+def is_it_win(player_one):
+    if check_row() or check_line() or check_diagonal():
+        if player_one:
             print('x win')
             return True
         else:
@@ -94,59 +102,57 @@ def IsItWin(playerOne):
             return True
     return False
 
-def IsPositionValid(positions):
-    #if position it's out of game boards returns false
+
+def is_position_valid(positions):
+    # if position it's out of game boards returns false
     if (positions[0] < 0 or positions[0] > 2) or (positions[1] < 0 or positions[1] > 2):
         return False
-    #if this cell is not empty then return false
-    elif (gameBoard[positions[0]][positions[1]] != ' '):
+    # if this cell is not empty then return false
+    elif gameBoard[positions[0]][positions[1]] != ' ':
         return False
     else:
         return True
 
-def PlayGame():
-    #list of game plan
+
+def play_game():
+    # list of game plan
     global gameBoard
-    gameBoard =  [[" "," "," "],[" "," "," "],[" "," "," "]]
+    gameBoard = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
     question = "Player do you want to play like x or o?"
-    #player one(X)
+    # player one(X)
     while True:
-        playerChoice = input(question).lower()
-        if playerChoice == 'x':
-            playerOne = True
+        player_choice = input(question).lower()
+        if player_choice == 'x':
+            player_one = True
             break
-        elif playerChoice == 'o':
-            playerOne = False
+        elif player_choice == 'o':
+            player_one = False
             break
     clear()
     print("TIC TAC TOE GAME STARTS!")
 
-    #Game is running till one player wins or game board is full and then it's a draw
+    # Game is running till one player wins or game board is full and then it's a draw
     while True:
-        positions = UserInput()
-        #put sign on cell selected by user if player one is true than it's a X otherwise O
-        if playerOne:
+        positions = user_input()
+        # put sign on cell selected by user if player one is true than it's a X otherwise O
+        if player_one:
             gameBoard[positions[0]][positions[1]] = 'x'
-        else: 
+        else:
             gameBoard[positions[0]][positions[1]] = 'o'
-        
-        #switch players
-        printGameBoard(gameBoard) 
-        if(IsItWin(playerOne)):
-            break
-        playerOne = not playerOne
 
-        if IsBoardFull():
+        # switch players
+        print_game_board(gameBoard)
+        if is_it_win(player_one):
             break
-    usersAnswer = (input("Do you want to play again? Y/N: ")).upper()
-    if usersAnswer == 'Y':
-        PlayGame()
+        player_one = not player_one
+
+        if is_it_draw():
+            break
+    users_answer = (input("Do you want to play again? Y/N: ")).upper()
+    if users_answer == 'Y':
+        play_game()
     else:
         print("Thanks for playing!")
 
-PlayGame()
 
-
-
-
-
+play_game()
