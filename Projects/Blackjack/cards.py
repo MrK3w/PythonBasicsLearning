072be9508e75de8add4a666bb14a606d453a31ec
MrK3w <jakubpietrus2000@gmail.com>
2021-02-10 14:15:14 +0100

    added function blackjack

--- Projects/Blackjack/cards.py
+++ Projects/Blackjack/cards.py
@@ -7,7 +7,7 @@ class Cards():
     cards = {1 :"ace",2:"two",3:"three",4:"four",5:"five",6:"six",7:"seven",8:"eight",9:"nine",10:"ten",11:"jack",12:"queen",13:"king"}
 
     def print_your_card(self,value_of_card):
-        print(f"Pyicked card is {self.cards[value_of_card]}")
+        print(f"Picked card is {self.cards[value_of_card]}")
 
     def get_card(self):
         value_of_card = randint(1,13)
@@ -42,33 +42,60 @@ def clear():
         _ = system('clear')
 
 def user_input():
-    choice = input("Do you want to draw another card? <Y> <N>: ").upper()
+    choice = input("<Y> <N>:").upper()
     if choice == 'Y':
         return True
     else:
         return False
 
+
+def start_game():
+    global players_win
+    global dealers_win
+    over = False
+    player_hand = PlayersHand("Jakub")
+    dealers_hand = PlayersHand()
+    while True:
+        player_hand.get_card()
+        if overleaped(player_hand.sum_of_cards):
+            over = True
+            print("dealer win player overlaped")
+            dealers_win +=1
+            break
+        dealers_hand.get_card()
+        if overleaped(dealers_hand.sum_of_cards):
+            over = True
+            print("user win dealer overlaped")
+            players_win += 1
+            break
+        print("Do you want to draw another card?",end="")
+        if user_input() is False:
+            break
+        clear()
+    print(f"{player_hand.user}'s total value is: {player_hand.sum_of_cards}")
+    print(f"{dealers_hand.user}'s total value is: {dealers_hand.sum_of_cards}\n")
+    if over == False:
+        while(dealers_hand.sum_of_cards < player_hand.sum_of_cards):
+            dealers_hand.get_card()
+            print(f"{player_hand.user}'s total value is: {player_hand.sum_of_cards}")
+            print(f"{dealers_hand.user}'s total value is: {dealers_hand.sum_of_cards}\n")
+            if overleaped(dealers_hand.sum_of_cards):
+                print("Player's wins")
+                players_win += 1
+    if player_hand.sum_of_cards > dealers_hand.sum_of_cards and over == False:
+        print("Player's wins")
+        players_win += 1
+    elif dealers_hand.sum_of_cards > player_hand.sum_of_cards and over == False:
+        print("Dealers wins")
+        dealers_win +=1
+
 cards_pack = Cards()
-player_hand = PlayersHand("Jakub")
-dealers_hand = PlayersHand()
-
-while True:
-    player_hand.get_card()
-    if overleaped(player_hand.sum_of_cards):
-        print("dealer win player overlaped 21")
-        exit()
-    dealers_hand.get_card()
-    if overleaped(dealers_hand.sum_of_cards):
-        print("user win dealer overlaped 21")
-        exit()
-    if user_input() is False:
+players_win = 0
+dealers_win = 0
+while(True): 
+    print(f"Players wins: {players_win} Dealers wins: {dealers_win} ")   
+    print("Do you want to play?", end= "")
+    if user_input() == False:
         break
-    clear()
+    start_game()
 
-print(f"{player_hand.user}'s total value is: {player_hand.sum_of_cards}")
-print(f"{dealers_hand.user}'s total value is: {dealers_hand.sum_of_cards}\n")
-
-if player_hand.sum_of_cards > dealers_hand.sum_of_cards:
-    print("Player's wins")
-else:
-    print("Dealers wins")
